f48025333c3179f1d43341d544e0cbf3ea8acef8
M-JM <jimmymiels@hotmail.com>
2019-12-29 18:18:31 +0100

    Added Share option in Menu

--- app/src/main/java/be/hub/jimmymiels/routefinder/Map/MapFragment.kt
+++ app/src/main/java/be/hub/jimmymiels/routefinder/Map/MapFragment.kt
@@ -2,10 +2,8 @@ package be.hub.jimmymiels.routefinder.Map
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 
@@ -32,10 +30,16 @@ class MapFragment : Fragment() {
         binding.newsearchButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mapFragment_to_titleFragment))
         binding.showrouteButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mapFragment_to_routeFragment))
 
+        setHasOptionsMenu(true)
+
         return binding.root
 
         //return inflater.inflate(R.layout.fragment_map, container, false)
     }
-
+    // Inflate the Menu xml
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
 
 }
