ef99ed8115a6fddf6ed54b83f65b46dc9eb5d25d
M-JM <jimmymiels@hotmail.com>
2020-01-03 00:55:55 +0100

    added binding for image

--- app/src/main/java/be/hub/jimmymiels/routefinder/map/MapFragment.kt
+++ app/src/main/java/be/hub/jimmymiels/routefinder/map/MapFragment.kt
@@ -3,6 +3,8 @@ package be.hub.jimmymiels.routefinder.map
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -14,6 +16,7 @@ import be.hub.jimmymiels.routefinder.databinding.FragmentMapBinding
 import be.hub.jimmymiels.routefinder.title.TitleViewModel
 import kotlinx.android.synthetic.main.fragment_map.*
 import kotlinx.android.synthetic.main.fragment_route.*
+import kotlin.math.absoluteValue
 
 /**
  * A simple [Fragment] subclass.
@@ -21,7 +24,7 @@ import kotlinx.android.synthetic.main.fragment_route.*
 class MapFragment : Fragment() {
 
     private lateinit var viewModel : TitleViewModel
-
+lateinit var  resultImage: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,12 +41,20 @@ class MapFragment : Fragment() {
         viewModel = ViewModelProviders.of(activity!!).get(TitleViewModel::class.java)
         binding.titleViewModel = viewModel
         binding.lifecycleOwner = this
+        resultImage =binding.imageView
+        Toast.makeText(context,viewModel.searchTerm.toString(), Toast.LENGTH_LONG).show()
 
         viewModel.searchTerm.observe(this,object : Observer<Any> {
             override fun onChanged(t: Any?) {
                 textView4.text = t!!.toString()
+                textView3.text = R.drawable.antwerpen.toString()
+                resultImage.setImageResource(viewModel.default)
+
+
             }
         })
+
+
 
         binding.newsearchButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mapFragment_to_titleFragment))
         binding.showrouteButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mapFragment_to_routeFragment))
