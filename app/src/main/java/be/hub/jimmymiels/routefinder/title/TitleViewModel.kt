06919791894db6336cbc01db33e5f1d35f123ef5
M-JM <jimmymiels@hotmail.com>
2020-01-03 00:55:06 +0100

    added function to set drawables to default variable (int) by searching against searchterm value (String)

--- app/src/main/java/be/hub/jimmymiels/routefinder/title/TitleViewModel.kt
+++ app/src/main/java/be/hub/jimmymiels/routefinder/title/TitleViewModel.kt
@@ -3,13 +3,11 @@ package be.hub.jimmymiels.routefinder.title
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.util.*
-
+import be.hub.jimmymiels.routefinder.R
+import kotlin.math.absoluteValue
 
 
 class TitleViewModel : ViewModel() {
-
-    data class Place(var city: String, var routegps: String )
 
 
     var searchTerm = MutableLiveData<String>()
@@ -17,20 +15,20 @@ class TitleViewModel : ViewModel() {
     private val _route = MutableLiveData<String>()
     val route: LiveData<String>
     get() = _route
-
-
-
-
-
-
+    var default = 0
 
   fun setSearchTerm(msg:String) {
     searchTerm.setValue(msg)
       _route.value = routes.getValue(searchTerm.value.toString())
 
-
 }
-
+    fun setImage() {
+     default = when (searchTerm.value.toString()) {
+        "Antwerpen" -> R.drawable.antwerpen
+        "Brussels" -> R.drawable.brussels
+        else -> R.drawable.share
+    }
+    }
 }
 
 
