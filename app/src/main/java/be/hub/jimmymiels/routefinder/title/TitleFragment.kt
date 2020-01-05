276b83d6d75852d19f142c6eebacf79187753289
M-JM <jimmymiels@hotmail.com>
2020-01-05 16:14:47 +0100

    Forces user to enter destination and throws back error in Edittext field when it is empty and user tries to search.

--- app/src/main/java/be/hub/jimmymiels/routefinder/title/TitleFragment.kt
+++ app/src/main/java/be/hub/jimmymiels/routefinder/title/TitleFragment.kt
@@ -2,11 +2,9 @@ package be.hub.jimmymiels.routefinder.title
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
@@ -14,25 +12,25 @@ import be.hub.jimmymiels.routefinder.R
 import be.hub.jimmymiels.routefinder.databinding.FragmentTitleBinding
 import kotlinx.android.synthetic.main.fragment_title.*
 
-/**
- * Fragment for the starting or title screen of the app
- */
+
 class TitleFragment : Fragment() {
 
-private lateinit var viewModel : TitleViewModel
+    private lateinit var viewModel: TitleViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
 
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_title,
             container,
-            false)
+            false
+        )
 
         // get viewModel
         // to call methods in viewmodel add viewModel. to methods
@@ -43,20 +41,24 @@ private lateinit var viewModel : TitleViewModel
         binding.lifecycleOwner = this
 
 
-
-
         // Navigation to fragment
 
-        binding.searchButton.setOnClickListener{ v: View ->
+        binding.searchButton.setOnClickListener { v: View ->
+            if (editText.text.isNotEmpty()) {
             viewModel!!.setSearchTerm(editText.text.toString())
             viewModel!!.setImage()
-            v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToRouteFragment())
+            v.findNavController()
+                .navigate(TitleFragmentDirections.actionTitleFragmentToRouteFragment())
+        }
+            else
+            { editText?.setError(" Please set destination")
+            }
         }
         return binding.root
     }
 
-        //return inflater.inflate(R.layout.fragment_title, container, false)
-    }
+
+}
 
 
 
